8ce10e5bbe10f741605496a811f53587d7545ec0
dkwestbr <dkwestbr@yahoo-inc.com>
2013-10-10 12:26:49 -0500

    Removing bean I tried to randomly add

--- src/main/java/dkwestbr/spring/autowired/example/config/AppConfig.java
+++ src/main/java/dkwestbr/spring/autowired/example/config/AppConfig.java
@@ -7,9 +7,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
-import dkwestbr.spring.autowired.example.IStringGetter;
-import dkwestbr.spring.autowired.example.stringGetter.impl.A;
-
 @Configuration
 @ComponentScan(basePackages = {"dkwestbr.spring.fun.stringGetter.**"})
 public class AppConfig {
@@ -18,11 +15,6 @@ public class AppConfig {
     private static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
     }
-    
-	@Bean
-	public IStringGetter getTheThing() {
-		return new A();
-	}
 
     @Configuration
     @PropertySource("classpath:configuration.properties")
