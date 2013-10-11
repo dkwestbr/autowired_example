e9aca9871484cf9375a33a3ba315b80b4841426a
dkwestbr <dkwestbr@yahoo-inc.com>
2013-10-11 13:08:39 -0500

    Adding profile to A

--- src/main/java/dkwestbr/spring/autowired/example/stringGetter/impl/A.java
+++ src/main/java/dkwestbr/spring/autowired/example/stringGetter/impl/A.java
@@ -1,11 +1,13 @@
 package dkwestbr.spring.autowired.example.stringGetter.impl;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import dkwestbr.spring.autowired.example.IStringGetter;
 
 @Component
+@Profile("!test")
 public class A implements IStringGetter {
 	
 	@Value("${my.property}")
